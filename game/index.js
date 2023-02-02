759aebc6347a20e7637c832cdeb91825937b052c
Mostafa <31211@ma-web.nl>
2023-02-02 16:44:19 +0100

    game

--- game/index.js
+++ game/index.js
@@ -1,9 +1,20 @@
+
+let game;
+function createPlayers(amountOfPlayers) {
+    game.start(amountOfPlayers);
+}
+function init()
+{
+    game = new Game();
+}
+
 class Player
 {
     constructor(index)
     {
         this.index = index;
         this.atTile = 0;
+        console.log(this.atTile);
         this.pawn = document.getElementsByClassName("pawn" + index)[0];
         this.pawn.style.display = "block";
     }
@@ -28,11 +39,12 @@ class Game
         this.boardDiv = document.getElementsByClassName("board")[0];
         this.boardoverlayDiv = document.getElementsByClassName("selectPlayers")[0];
 
-        console.log(this.rollDiv);
-
         this.tiles = [];
         this.players = [];
+        console.log(this.tiles)
+        console.log(this.players)
         this.playerTurn = 0;
+        console.log(this.playerTurn)
         this.setupBoard();
     }
     setupBoard()
@@ -61,31 +73,80 @@ class Game
             this.tiles.push(tile);
 
         }
+        this.setupGotos();
     }
 
     setupGotos()
-    {
-
+    {   //let op! deze tegelnummers beginnen 1
+        let goto = [[6, 14], [16, 4], [17, 23], [27, 33], [29, 10], [38, 43], [39, 20], [45, 34]];
+        for (var i = 0; i < goto.length; i++)
+        {
+            let element = goto[i];
+            let start = element[0] - 1;
+            let end = element[1] - 1;
+            let tile = this.tiles[start];
+            tile.goto = end;
+        }
     }
     start(amountOfPlayers)
-    {
-
+    {   
+        this.selectplayersDiv.style.display = "none";
+        this.winnerDiv.style.display = "none";
+        for (let i = 0; i < 4; i++) {
+            document.getElementsByClassName("pawn" + i)[0].style.display = "none";
+        }
+        for (let i = 0; i < amountOfPlayers; i++) {
+            this.players.push(new Player(i));
+        }
+        this.playerTurn = -1;
+        this.moveToNextPlayer();
     }
+
     moveToNextPlayer()
     {
-
+        this.playerTurn = (this.playerTurn + 1) % this.players.length;
+        this.playerturnDiv.textContent = "Player " + (this.playerTurn + 1) + "'s turn.";
+        this.rollDiv.style.display = "block";
+        this.draw();
     }
+
     draw()
     {
-
+        for (let i = 0; i < this.players.length; i++) {
+            this.setPawn(i, this.players[i].atTile);
+        }
     }
-    roll()
-    {
 
+    setPawn(playerI, atTile) {
+        let player = this.players[playerI];
+        player.atTile = atTile;
+        let tile = this.tiles[atTile];
+        player.pawn.style.left = tile.div.style.left;
+        player.pawn.style.top = tile.div.style.top;
     }
-    setPawn(playerI, atTile)
-    {
 
+    roll() 
+    {
+        let roll = Math.floor(Math.random() * 6) + 1;
+        console.log(roll)
+        this.rollDiv.style.backgroundImage = "url(img/dice" + roll + ".png)";
+        let player = this.players[this.playerTurn];
+        let atTile = player.atTile;
+        console.log(player);
+        atTile = (atTile + roll) % this.tiles.length;
+        if (atTile >= this.tiles.length-1) {
+            this.winnerDiv.textContent = "Player " + (this.playerTurn + 1) + " wins!";
+            this.winnerDiv.style.display = "block";
+        } 
+        else {
+            let tile = this.tiles[atTile];
+            if (tile.goto !== -1) {
+                atTile = tile.goto;
+            }
+            this.setPawn(this.playerTurn, atTile);
+            this.draw();
+            this.moveToNextPlayer();
+        }
     }
     makeBoardDiv(x,y,tileDisplayNumber)
     {
@@ -93,14 +154,11 @@ class Game
         div.className = "tile";
         div.style.left = x + "px";
         div.style.top = y + "px";
-        div.textContent = tileDisplayNumber;
-        console.log(this.boardDiv);        
+        //Jerukpurut404//
+        div.textContent = tileDisplayNumber;      
         this.boardDiv.appendChild(div);
 
         return div;
     }
-}
-function init()
-{
-    let game = new Game();
+
 }
